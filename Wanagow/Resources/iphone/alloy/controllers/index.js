83916ae16acac098e95f36ca6036fe38f50cad30
Lenguajes de Programacion <hectorcamposalonso@hotmail.com>
2014-06-09 14:39:38 -0500

    Modulo Principal
    Eventos del usuario

--- Wanagow/Resources/iphone/alloy/controllers/index.js
+++ Wanagow/Resources/iphone/alloy/controllers/index.js
@@ -131,7 +131,7 @@ function Controller() {
     var exports = {};
     var __defers = {};
     $.__views.index = Ti.UI.createWindow({
-        backgroundColor: "white",
+        backgroundColor: "#f4ce00",
         id: "index"
     });
     $.__views.index && $.addTopLevelView($.__views.index);
@@ -144,20 +144,42 @@ function Controller() {
     });
     $.__views.index.add($.__views.label);
     $.__views.label1 = Ti.UI.createLabel({
-        width: Ti.UI.SIZE,
-        height: Ti.UI.SIZE,
+        width: 400,
+        height: 45,
         color: "#000",
+        layout: "center",
+        Color: "black",
+        top: 550,
+        left: "38%",
         text: "Desliza para aprender mas.",
         id: "label1"
     });
     $.__views.index.add($.__views.label1);
     $.__views.email = Ti.UI.createTextField({
+        width: 300,
+        right: 50,
+        left: 250,
+        height: 45,
+        top: 600,
+        borderRadius: 10,
+        borderColor: "black",
+        borderWidth: 2,
+        hintText: "Email",
         id: "email",
         borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
         color: "black"
     });
     $.__views.index.add($.__views.email);
     $.__views.password = Ti.UI.createTextField({
+        width: 300,
+        right: 50,
+        left: 250,
+        height: 45,
+        top: 650,
+        borderRadius: 10,
+        borderColor: "black",
+        borderWidth: 2,
+        hintText: "Password",
         id: "password",
         passwordMask: "true",
         focusable: "place",
@@ -166,21 +188,47 @@ function Controller() {
     });
     $.__views.index.add($.__views.password);
     $.__views.btn1 = Ti.UI.createButton({
+        width: 200,
+        right: 100,
+        left: 300,
+        borderRadius: 15,
+        backgroundColor: "#515050",
+        height: 50,
+        top: 700,
+        font: {
+            fontFamily: "Helvetica Neue"
+        },
+        color: "white",
         title: "Entrar",
         id: "btn1"
     });
     $.__views.index.add($.__views.btn1);
     login ? $.__views.btn1.addEventListener("click", login) : __defers["$.__views.btn1!click!login"] = true;
     $.__views.btn2 = Ti.UI.createButton({
+        width: 200,
+        right: 100,
+        left: 300,
+        borderRadius: 10,
+        backgroundColor: "#515050",
+        height: 50,
+        top: 800,
+        font: {
+            fontFamily: "Helvetica Neue"
+        },
+        color: "white",
         title: "Registrarse",
         id: "btn2"
     });
     $.__views.index.add($.__views.btn2);
     btn_2 ? $.__views.btn2.addEventListener("click", btn_2) : __defers["$.__views.btn2!click!btn_2"] = true;
     $.__views.label2 = Ti.UI.createLabel({
-        width: Ti.UI.SIZE,
-        height: Ti.UI.SIZE,
+        width: 400,
+        height: 45,
         color: "#000",
+        layout: "center",
+        Color: "black",
+        top: 750,
+        left: "38%",
         text: "Si todavia no tienes una cuenta",
         id: "label2"
     });
